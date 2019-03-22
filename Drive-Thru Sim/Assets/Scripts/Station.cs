5d87b4a579889f236110a82ad69a7cb0d31eb0bf
KatWeis <kxw4589@g.rit.edu>
2019-03-22 10:52:26 -0400

    Began Station Programming

---
+++ Drive-Thru Sim/Assets/Scripts/Station.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PlayerScript requires the GameObject to have a Rigidbody component
+[RequireComponent(typeof(BoxCollider2D))]
+public class Station : MonoBehaviour
+{
+    protected float prepTime; // How long the station takes to prepare the food
+    protected float overflowTime; // How long the station can be left after the item completes before it gets ruined
+    protected bool inUse; // If this station is currently being used
+
+    public GameObject item; // The prefab of the item that will be prepared at this station
+
+    protected BoxCollider2D activator; // Collider around the station for triggering
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activator = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void TriggerStation()
+    {
+
+    }
+
+    protected void HireWorker()
+    {
+
+    }
+}
---
+++ Drive-Thru Sim/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    private bool isNight; // Is it the night phase where player can upgrade
+
+    public bool IsNight
+    {
+        get { return isNight; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
