06cdaaea6d129136be7d12c64dffedc72119cecb
Matt <mcl1601>
2019-03-22 10:47:23 -0400

    Began workin on Orders and OrderManager. Tickets appear on UI and countdown, then delete

---
+++ Drive-Thru Sim/Assets/Scripts/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Order
+{
+    public List<Item> items;
+    public float timer;
+    private float totalTime;
+    public GameObject ticketUI;
+
+    public Order(List<Item> i, float time)
+    {
+        items = i;
+        timer = time;
+        totalTime = time;
+    }
+
+    public bool UpdateTimer()
+    {
+        timer -= Time.deltaTime;
+        float percent = timer / totalTime;
+        ticketUI.transform.GetChild(2).GetComponent<Image>().fillAmount = percent;
+        return timer < 0;
+    }
+}
