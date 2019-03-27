845eaa3c18cabe8c74a863ea7c2e89b0a2b0ece9
KatWeis <kxw4589@g.rit.edu>
2019-03-27 14:01:12 -0400

    Finished up station coding. Added in some textures for the completed  products to differentiate them from the counter

--- Drive-Thru Sim/Assets/Scripts/Station.cs
+++ Drive-Thru Sim/Assets/Scripts/Station.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // PlayerScript requires the GameObject to have a Rigidbody component
 [RequireComponent(typeof(BoxCollider))]
 public class Station : MonoBehaviour
 {
-    protected float prepTime; // How long the station takes to prepare the food
-    protected float overflowTime; // How long the station can be left after the item completes before it gets ruined
+    public float prepTime; // How long the station takes to prepare the food
+    public float overflowTime; // How long the station can be left after the item completes before it gets ruined
     protected float timer;
+
+    public Image timerVis; // UI component to represent the timer on prepping the food
+
     protected bool inUse; // If this station is currently being used
 
     public GameObject item; // The prefab of the item that will be prepared at this station
@@ -29,6 +33,7 @@ public class Station : MonoBehaviour
         if(inUse)
         {
             timer += Time.deltaTime;
+            timerVis.fillAmount = (timer / prepTime);
         }
     }
 
@@ -37,8 +42,9 @@ public class Station : MonoBehaviour
         inUse = true;
         timer = 0.0f;
 
-        GameObject itemInit = Instantiate(item, gameObject.transform);
-        itemInit.transform.position = new Vector3(0, gameObject.transform.localScale.y/2, 0);
+        GameObject itemInit = Instantiate(item, new Vector3(0, gameObject.transform.localScale.y / 2, 0), Quaternion.identity);
+        itemInit.transform.parent = gameObject.transform;
+        itemInit.transform.localPosition = new Vector3(0f, itemInit.transform.position.y, 0f);
 
         Debug.Log("Starting station");
     }
@@ -48,7 +54,8 @@ public class Station : MonoBehaviour
         inUse = false;
 
         gameObject.transform.GetChild(0).SetParent(player);
-        
+
+        timerVis.fillAmount = 0;
 
         Debug.Log("Picking up from station");
     }
