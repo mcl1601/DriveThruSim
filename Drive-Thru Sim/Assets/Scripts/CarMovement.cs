9ed75756a504aa17a89a050a912b6b1513f374de
Mike O'Donnell <mxo7550@rit.edu>
2019-04-02 18:54:10 -0400

    WE'VE GOT THE CAR TO ROTATE LET'S GOOOOO

--- Drive-Thru Sim/Assets/Scripts/CarMovement.cs
+++ Drive-Thru Sim/Assets/Scripts/CarMovement.cs
@@ -20,8 +20,13 @@ public class CarMovement : MonoBehaviour
         //Keep moving it down the driveway
         position += (transform.up * Time.deltaTime * speed);
         transform.position = position;
-        if(rotating && transform.rotation.y > -90f)
+        if(rotating)
             transform.Rotate(new Vector3(0, -10f, 0) * (2.5f * Time.deltaTime), Space.World);
+        if (transform.rotation.eulerAngles.y <= 270f && transform.rotation.eulerAngles.y > 0)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+            rotating = false;
+        }
     }
     void OnTriggerEnter(Collider other)
     {
